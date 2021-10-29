a57443ceb8b3fc3a46d5d64c48a6aa90fdfa3f6d
user1 <user1@gmail.com>
2021-10-29 18:52:57 +0530

    shell script changed

--- shell-script.sh
+++ shell-script.sh
@@ -1,41 +1,22 @@
-#!/bin/bash
-sudo yum update -y
-sudo chown -R ec2-user:ec2-user /opt
+udo chown -R ec2-user:ec2-user /opt
 cd /opt
 sudo yum install wget -y
 wget https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u282-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u282b08.tar.gz
-tar -zxvf OpenJDK8U-jdk_x64_linux_hotspot_8u282b08.tar.gz
-mv jdk8u282-b08 java8
+tar -zxvf OpenJDK8U-jdk_x64_linux_hotspot_8u282b08.tar.gz 
+mv jdk8u282-b08/ java8
 sudo sed -i '$a export JAVA_HOME=/opt/java8' /etc/profile
 sudo sed -i '$a export PATH=$PATH:/opt/java8/bin' /etc/profile
-source /etc/profile
-cd 
-cd /opt
-wget https://mirrors.estointernet.in/apache/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.tar.gz
-tar -zxvf apache-maven-3.8.1-bin.tar.gz
-mv apache-maven-3.8.1 maven3
-sudo sed -i '$a export M2_HOME=/opt/maven3' /etc/profile
-sudo sed -i '$a export PATH=$PATH:/opt/maven3/bin' /etc/profile
-source /etc/profile
-cd
-cd /opt
-sudo yum install git -y
-sudo git clone https://github.com/sriison/spring-petclinic-1
-cd spring-petclinic-1/
-sudo mvn clean package
-cd
-cd /opt
-wget https://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.65/bin/apache-tomcat-8.5.65.tar.gz
-tar -zxvf apache-tomcat-8.5.65.tar.gz
-mv apache-tomcat-8.5.65 tomcat8
-cd tomcat8/
-cd bin/
-sh startup.sh 
-cd
-cd /opt/spring-petclinic-1/target
-cp petclinic.war /opt/tomcat8/webapps
-cd
-cd /opt/tomcat8
-cd bin/
-sh shutdown.sh
-sh startup.sh
+#wget https://dlcdn.apache.org/maven/maven-3/3.8.3/binaries/apache-maven-3.8.3-bin.tar.gz --no-check-certificate
+#mv apache-maven-3.8.3 maven3
+#sudo sed -i '$a export M2_HOME=/opt/maven3' /etc/profile
+#sudo sed -i '$a export PATH=$PATH:/opt/maven3/bin' /etc/profile
+#source /etc/profile
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.72/bin/apache-tomcat-8.5.72.tar.gz
+tar -zxvf apache-tomcat-8.5.72.tar.gz 
+mv apache-tomcat-8.5.72 tomcat8
+rm -rf *.tar.gz
+cd /opt/tomcat8/webapps
+wget https://get.jenkins.io/war/2.314/jenkins.war
+sudo yum install fontconfig -y 
+sudo chown -R ec2-user:ec2-user /opt
+sh /opt/tomcat8/bin/startup.sh 
